55496e1424e10a2f92f0d36907dc1b7002e466a0
Ernesto Ruiz <eruiz@wbapps.com>
2022-09-16 13:18:40 +0000

    Ajustes varios

--- sig_mineros/sig_mineros/doctype/entrega_y_pago_de_mineral/entrega_y_pago_de_mineral_list.js
+++ sig_mineros/sig_mineros/doctype/entrega_y_pago_de_mineral/entrega_y_pago_de_mineral_list.js
@@ -1,11 +1,5 @@
 frappe.listview_settings['Entrega y Pago de Mineral'] = {
 	
-	
-	onload(listview) {
-		$("[data-view=Kanban]").addClass("hide");
-		$("[data-view=Gantt]").addClass("hide");
-	},
-	
 	//hide_name_column: true,
 	add_fields: ["estado_del_mineral"],
 	get_indicator: function (doc) {
@@ -16,14 +10,18 @@ frappe.listview_settings['Entrega y Pago de Mineral'] = {
 			return [__("Pendiente de Resultados"), "grey", "estado_del_mineral,=,Pendiente de Resultados"];
 		} else if (doc.estado_del_mineral === "Aceptado") {
 			return [__("Aceptado"), "green", "estado_del_mineral,=,Aceptado"];
+			} else if (doc.estado_del_mineral === "Remuestreo/Aceptado"	) {
+			return [__("R/Aceptado"), "green", "estado_del_mineral,=,Remuestreo/Aceptado"];
 		} else if (doc.estado_del_mineral === "Remuestreo") {
 			return [__("Remuestreo"), "blue", "estado_del_mineral,=,Remuestreo"];
-		}  else if (doc.estado_del_mineral === "Rechazado por Baja Ley") {
-			return [__("Rechazado por Baja Ley"), "red", "estado_del_mineral,=,Rechazado por Baja Ley"];
-		}  else if (doc.estado_del_mineral === "Rechazado por Valores Dispersos") {
-			return [__("Rechazado por Valores Dispersos"), "red", "estado_del_mineral,=,Rechazado por Valores Dispersos"];
+		}  else if (doc.estado_del_mineral === "Remuestreo/Rechazado") {
+			return [__("R/Rechazado"), "red", "estado_del_mineral,=,Remuestreo/Rechazado"];
+		}  else if (doc.estado_del_mineral === "Rechazado por HEMCO") {
+			return [__("Rechazado por HEMCO"), "red", "estado_del_mineral,=,Rechazado por HEMCO"];
 		}  else if (doc.estado_del_mineral === "Rechazado por Minero Artesanal") {
 			return [__("Rechazado por Minero Artesanal"), "orange", "estado_del_mineral,=,Rechazado por Minero Artesanal"];
+		} else if (doc.estado_del_mineral === "Remuestreo/No Retirado") {
+			return [__("R/No Retirado"), "orange", "estado_del_mineral,=,Remuestreo/No Retirado"];
 		} 
 	}, 
 };
--- sig_mineros/sig_mineros/doctype/registro_de_accidentes/registro_de_accidentes_list.js
+++ sig_mineros/sig_mineros/doctype/registro_de_accidentes/registro_de_accidentes_list.js
@@ -1,11 +1,5 @@
 frappe.listview_settings['Registro de Accidentes'] = {
 	
-	
-	onload(listview) {
-		$("[data-view=Calendar]").addClass("hide");
-		$("[data-view=Kanban]").addClass("hide");
-	},
-	
 	hide_name_column: true,
 	add_fields: ["tipo_accidente"],
 	get_indicator: function (doc) {
@@ -16,7 +10,8 @@ frappe.listview_settings['Registro de Accidentes'] = {
 		} else if (doc.tipo_accidente === "Muy Grave") {
 			return [__("Muy Grave"), "red", "tipo_accidente,=,Muy Grave"];
 		}  else if (doc.tipo_accidente === "Mortal") {
-			return [__("Mortal"), "black", "tipo_accidente,=,Mortal"];
+			return [__("Mortal"), "gray", "tipo_accidente,=,Mortal"];
 		} 
 	}, 
+	
 };
